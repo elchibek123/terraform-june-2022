dd9ff832f3d65935686e1df24bbf139219febe93
elchibek <elchibek.kamalov.com>
2022-07-18 19:38:40 +0000

    added session-5

---
+++ session-5/backend.tf
@@ -0,0 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-session-5-remote-backend-bucket"
+    key            = "session-5/terraform.tfstate"
+    region         = "us-west-2"
+    dynamodb_table = "terraform-june-2022-state-lock-table"
+  }
+}
+
+# Terraform State Lock
+# Terraform State Lock prevents from the accidental Apply or Destroy command or State change.
+# It will make sure one apply or destroy at the time.
+# Terraform State Lock uses DynamoDB table to lock every single execution.
---
+++ session-5/variables.tf
@@ -0,0 +1,23 @@
+variable "instance_type" {
+  type        = string
+  description = "This is a size of EC2"
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  type        = string
+  description = "This is my Public Key"
+  default     = "MyMacKey"
+}
+
+variable "env" {
+  type        = string
+  description = "This variable represents environment"
+  default     = "dev"
+}
+
+variable "ingress_ports" {
+  type        = list(string)
+  description = "This is a list of ports for Ingress Rule"
+  default     = ["22", "80", "443", "3306", "5432"]
+}
